1854ba0334fec74537a63d6d4b307ec044c9ad5d
hieudangvietcode <106416768+hieudangvietcode@users.noreply.github.com>
2022-11-26 10:00:28 +0700

    no message

--- DuAnBanGiay/src/duanbangiay/domainmodel/LoaiSP.java
+++ DuAnBanGiay/src/duanbangiay/domainmodel/LoaiSP.java
@@ -5,10 +5,95 @@
 
 package duanbangiay.domainmodel;
 
+import java.util.Date;
+
 /**
  * 
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class LoaiSP {
+private String id;
+private String ma;
+private String ten;
+private int trangThai;
+private Date ngayTao;
+private Date ngaySua;
+
+    public LoaiSP() {
+    }
+
+    public LoaiSP(String id, String ma, String ten, int trangThai, Date ngayTao, Date ngaySua) {
+        this.id = id;
+        this.ma = ma;
+        this.ten = ten;
+        this.trangThai = trangThai;
+        this.ngayTao = ngayTao;
+        this.ngaySua = ngaySua;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public Date getNgayTao() {
+        return ngayTao;
+    }
+
+    public void setNgayTao(Date ngayTao) {
+        this.ngayTao = ngayTao;
+    }
+
+    public Date getNgaySua() {
+        return ngaySua;
+    }
+
+    public void setNgaySua(Date ngaySua) {
+        this.ngaySua = ngaySua;
+    }
+public String chuyenTrangthai(){
+    if (trangThai==1) {
+        return "Đẹp";
+    }else{
+        return "OK";
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 }
